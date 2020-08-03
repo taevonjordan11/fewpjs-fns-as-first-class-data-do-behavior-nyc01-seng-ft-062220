680fc506e155cf135fb06760e8b90ad4852a84b2
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-08-03 17:25:13 -0400

    done

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10)
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
